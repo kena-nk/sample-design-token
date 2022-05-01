1ef177582c8f8f9cce744f089c1ebb72a4540f5e
kena-nk <keina_nakamoto@rebuild-inc.co.jp>
2022-05-01 17:24:13 +0900

    sassとjsファイルを書き出した

---
+++ src/tokens/tokens.d.ts
@@ -0,0 +1,30 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 01 May 2022 08:23:02 GMT
+ */
+
+export default tokens;
+
+declare interface DesignToken {
+  value: any;
+  name?: string;
+  comment?: string;
+  themeable?: boolean;
+  attributes?: {
+    category?: string;
+    type?: string;
+    item?: string;
+    subitem?: string;
+    state?: string;
+    [key: string]: any;
+  };
+  [key: string]: any;
+}
+
+declare const tokens: {
+  "Gray": {
+    "0": DesignToken,
+    "50": DesignToken,
+    "100": DesignToken
+  }
+}
---
+++ src/tokens/tokens.js
@@ -0,0 +1,66 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 01 May 2022 08:23:02 GMT
+ */
+
+module.exports = {
+  "Gray": {
+    "0": {
+      "value": "#FFF",
+      "type": "color",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "#FFF",
+        "type": "color"
+      },
+      "name": "Gray0",
+      "attributes": {
+        "category": "Gray",
+        "type": "0"
+      },
+      "path": [
+        "Gray",
+        "0"
+      ]
+    },
+    "50": {
+      "value": "#D9D9D9",
+      "type": "color",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "#D9D9D9",
+        "type": "color"
+      },
+      "name": "Gray50",
+      "attributes": {
+        "category": "Gray",
+        "type": "50"
+      },
+      "path": [
+        "Gray",
+        "50"
+      ]
+    },
+    "100": {
+      "value": "#000",
+      "type": "color",
+      "filePath": "output.json",
+      "isSource": true,
+      "original": {
+        "value": "#000",
+        "type": "color"
+      },
+      "name": "Gray100",
+      "attributes": {
+        "category": "Gray",
+        "type": "100"
+      },
+      "path": [
+        "Gray",
+        "100"
+      ]
+    }
+  }
+};
