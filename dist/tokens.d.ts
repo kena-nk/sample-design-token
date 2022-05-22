6625027667d0b625cf597bb942d3ff405431edc3
kena-nk <keina_nakamoto@rebuild-inc.co.jp>
2022-05-22 20:25:58 +0900

    distをgitの管理下に置くようにした

---
+++ dist/tokens.d.ts
@@ -0,0 +1,42 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 21 May 2022 08:47:40 GMT
+ */
+
+export default tokens;
+
+declare interface DesignToken {
+  value: any;
+  name?: string;
+  comment?: string;
+  themeable?: boolean;
+  attributes?: {
+    category?: string;
+    type?: string;
+    item?: string;
+    subitem?: string;
+    state?: string;
+    [key: string]: any;
+  };
+  [key: string]: any;
+}
+
+declare const tokens: {
+  "colors": {
+    "black": DesignToken,
+    "white": DesignToken,
+    "red": DesignToken
+  },
+  "space": {
+    "xs": DesignToken,
+    "sm": DesignToken,
+    "lg": DesignToken,
+    "xl": DesignToken
+  },
+  "borderRadius": {
+    "xs": DesignToken,
+    "sm": DesignToken,
+    "lg": DesignToken,
+    "xl": DesignToken
+  }
+}
---
+++ dist/tokens.js
@@ -0,0 +1,230 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 21 May 2022 08:47:40 GMT
+ */
+
+module.exports = {
+  "colors": {
+    "black": {
+      "value": "#000000",
+      "type": "color",
+      "filePath": "tokens/output.json",
+      "isSource": true,
+      "original": {
+        "value": "#000000",
+        "type": "color"
+      },
+      "name": "ColorsBlack",
+      "attributes": {
+        "category": "colors",
+        "type": "black"
+      },
+      "path": [
+        "colors",
+        "black"
+      ]
+    },
+    "white": {
+      "value": "#ffffff",
+      "type": "color",
+      "filePath": "tokens/output.json",
+      "isSource": true,
+      "original": {
+        "value": "#ffffff",
+        "type": "color"
+      },
+      "name": "ColorsWhite",
+      "attributes": {
+        "category": "colors",
+        "type": "white"
+      },
+      "path": [
+        "colors",
+        "white"
+      ]
+    },
+    "red": {
+      "value": "#df0000",
+      "type": "color",
+      "filePath": "tokens/output.json",
+      "isSource": true,
+      "original": {
+        "value": "#df0000",
+        "type": "color"
+      },
+      "name": "ColorsRed",
+      "attributes": {
+        "category": "colors",
+        "type": "red"
+      },
+      "path": [
+        "colors",
+        "red"
+      ]
+    }
+  },
+  "space": {
+    "xs": {
+      "value": 2,
+      "type": "spacing",
+      "description": "最小",
+      "filePath": "tokens/output.json",
+      "isSource": true,
+      "original": {
+        "value": 2,
+        "type": "spacing",
+        "description": "最小"
+      },
+      "name": "SpaceXs",
+      "attributes": {
+        "category": "space",
+        "type": "xs"
+      },
+      "path": [
+        "space",
+        "xs"
+      ]
+    },
+    "sm": {
+      "value": 4,
+      "type": "spacing",
+      "description": "小",
+      "filePath": "tokens/output.json",
+      "isSource": true,
+      "original": {
+        "value": 4,
+        "type": "spacing",
+        "description": "小"
+      },
+      "name": "SpaceSm",
+      "attributes": {
+        "category": "space",
+        "type": "sm"
+      },
+      "path": [
+        "space",
+        "sm"
+      ]
+    },
+    "lg": {
+      "value": 8,
+      "type": "spacing",
+      "description": "大",
+      "filePath": "tokens/output.json",
+      "isSource": true,
+      "original": {
+        "value": 8,
+        "type": "spacing",
+        "description": "大"
+      },
+      "name": "SpaceLg",
+      "attributes": {
+        "category": "space",
+        "type": "lg"
+      },
+      "path": [
+        "space",
+        "lg"
+      ]
+    },
+    "xl": {
+      "value": 16,
+      "type": "spacing",
+      "description": "最大",
+      "filePath": "tokens/output.json",
+      "isSource": true,
+      "original": {
+        "value": 16,
+        "type": "spacing",
+        "description": "最大"
+      },
+      "name": "SpaceXl",
+      "attributes": {
+        "category": "space",
+        "type": "xl"
+      },
+      "path": [
+        "space",
+        "xl"
+      ]
+    }
+  },
+  "borderRadius": {
+    "xs": {
+      "value": 2,
+      "type": "borderRadius",
+      "filePath": "tokens/output.json",
+      "isSource": true,
+      "original": {
+        "value": 2,
+        "type": "borderRadius"
+      },
+      "name": "BorderRadiusXs",
+      "attributes": {
+        "category": "borderRadius",
+        "type": "xs"
+      },
+      "path": [
+        "borderRadius",
+        "xs"
+      ]
+    },
+    "sm": {
+      "value": 4,
+      "type": "borderRadius",
+      "filePath": "tokens/output.json",
+      "isSource": true,
+      "original": {
+        "value": 4,
+        "type": "borderRadius"
+      },
+      "name": "BorderRadiusSm",
+      "attributes": {
+        "category": "borderRadius",
+        "type": "sm"
+      },
+      "path": [
+        "borderRadius",
+        "sm"
+      ]
+    },
+    "lg": {
+      "value": 8,
+      "type": "borderRadius",
+      "filePath": "tokens/output.json",
+      "isSource": true,
+      "original": {
+        "value": 8,
+        "type": "borderRadius"
+      },
+      "name": "BorderRadiusLg",
+      "attributes": {
+        "category": "borderRadius",
+        "type": "lg"
+      },
+      "path": [
+        "borderRadius",
+        "lg"
+      ]
+    },
+    "xl": {
+      "value": 16,
+      "type": "borderRadius",
+      "filePath": "tokens/output.json",
+      "isSource": true,
+      "original": {
+        "value": 16,
+        "type": "borderRadius"
+      },
+      "name": "BorderRadiusXl",
+      "attributes": {
+        "category": "borderRadius",
+        "type": "xl"
+      },
+      "path": [
+        "borderRadius",
+        "xl"
+      ]
+    }
+  }
+};
