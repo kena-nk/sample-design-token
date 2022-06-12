efd59e6cd742ff56dc8119117b6da927f552589e
GitHub Action <action@github.com>
2022-06-12 10:41:15 +0000

    update

--- dist/tokens.d.ts
+++ dist/tokens.d.ts
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Sun, 05 Jun 2022 07:18:22 GMT
+ * Generated on Sun, 12 Jun 2022 10:41:15 GMT
  */
 
 export default tokens;
@@ -29,7 +29,8 @@ declare const tokens: {
     "pink": DesignToken,
     "blue": DesignToken,
     "orange": DesignToken,
-    "yellow": DesignToken
+    "yellow": DesignToken,
+    "purple": DesignToken
   },
   "space": {
     "xs": DesignToken,
--- dist/tokens.js
+++ dist/tokens.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Sun, 05 Jun 2022 07:18:22 GMT
+ * Generated on Sun, 12 Jun 2022 10:41:15 GMT
  */
 
 module.exports = {
@@ -136,6 +136,25 @@ module.exports = {
       "path": [
         "colors",
         "yellow"
+      ]
+    },
+    "purple": {
+      "value": "#af15f8",
+      "type": "color",
+      "filePath": "tokens/output.json",
+      "isSource": true,
+      "original": {
+        "value": "#af15f8",
+        "type": "color"
+      },
+      "name": "ColorsPurple",
+      "attributes": {
+        "category": "colors",
+        "type": "purple"
+      },
+      "path": [
+        "colors",
+        "purple"
       ]
     }
   },
